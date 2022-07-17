864ad1423c2b8291f9ab1d89d2bbf1b4b3732587
Gustavo Konrad <g.konrad@outlook.com>
2022-07-17 13:58:23 -0300

    fix: fixing CI, removing useless eslint rules

--- src/vite-plugin-zod-env.ts
+++ src/vite-plugin-zod-env.ts
@@ -1,11 +1,10 @@
 import type { AnyZodObject, ZodFormattedError } from 'zod'
 import { Plugin, loadEnv } from 'vite'
 
-const formatErrors = (errors: ZodFormattedError<Map<string, string>, string>) =>
+const formatErrors = (errors: ZodFormattedError<Map<string, string>>) =>
   Object.entries(errors)
     .map(([name, value]) => {
-      if (value && '_errors' in value)
-        return `${name}: ${value._errors.join(', ')}\n`
+      if ('_errors' in value) return `${name}: ${value._errors.join(', ')}\n`
     })
     .filter(Boolean)
 
